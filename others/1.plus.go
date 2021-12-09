bb6f8968b6d963f3b3b9e7638453d22fc5dd2d0b
xingshanghe <xingshanghe@gmail.com>
2021-12-09 15:21:05 +0800

    add

--- others/1.plus.go
+++ others/1.plus.go
@@ -10,6 +10,21 @@ package main
 
 /**
 不用 "+"号实现加法
+所涉及基础知识储备：
+
+	位运算：
+	&(与): 	两位都为1时，结果为1
+	|(或): 	两位都为0时，结果为0
+	^(异或):	两位都为0时，结果为1；另 a^a=0,a^0=a
+
+	计算机中 0和1相加，几种情况（二进制情况）:
+	0 + 0 = 00
+	0 + 1 = 01
+	1 + 0 = 01
+	1 + 1 = 10
+
+	不考虑进位相加: 	a^b
+	进位值: 			(a&b)<<1
 */
 
 // add1 实现 一位数 只能实现0和1之间的加法
@@ -20,7 +35,7 @@ func add1(a, b int) int {
 }
 
 // add
-// @Description:
+// @Description: 加法，递归实现
 // @Date: 2021-12-09 13:15:43
 // @param a
 // @param b
@@ -32,5 +47,19 @@ func add(a, b int) int {
 	}
 	carry := (a & b) << 1 // 进位的值
 	unCarry := a ^ b      // 不考虑进位，相加的值
-	return add(carry, unCarry)
+	return add(unCarry, carry)
+}
+
+func addNoRecurrence(a, b int) int {
+	for {
+		if b == 0 {
+			break
+		}
+		unCarry := a ^ b
+		carry := (a & b) << 1
+
+		a = unCarry
+		b = carry
+	}
+	return a
 }
--- others/1.plus_test.go
+++ others/1.plus_test.go
@@ -10,7 +10,7 @@ package main
 
 import "testing"
 
-func TestPlus(t *testing.T) {
+func TestAdd(t *testing.T) {
 	inputs := [][2]int{
 		{1, 2},
 		{3, 0},
@@ -23,6 +23,25 @@ func TestPlus(t *testing.T) {
 	}
 	for k, input := range inputs {
 		sum := add(input[0], input[1])
+		if outputs[k] != sum {
+			t.Error(input)
+		}
+	}
+}
+
+func TestAddNoRecurrence(t *testing.T) {
+	inputs := [][2]int{
+		{1, 2},
+		{3, 0},
+		{123, 321},
+		{99, 1},
+		{-17, -3},
+	}
+	outputs := []int{
+		3, 3, 444, 100, -20,
+	}
+	for k, input := range inputs {
+		sum := addNoRecurrence(input[0], input[1])
 		if outputs[k] != sum {
 			t.Error(input)
 		}
